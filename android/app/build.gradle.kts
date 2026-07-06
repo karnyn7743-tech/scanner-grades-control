2ff67a6a7225751b20196fa1cffbd6164c7fe62e
أبو الخضر البعيثي <karnyn7743@gmail.com>
2026-07-06 20:22:57 +0300

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.macke_exampapers_withcode"
+    namespace = "com.example.macke_exampapers"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.macke_exampapers_withcode"
+        applicationId = "com.example.macke_exampapers"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
