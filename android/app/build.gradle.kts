496eef5f9a481e9d91f7dad048dbc909276b7116
أبو الخضر البعيثي <karnyn7743@gmail.com>
2026-08-07 23:12:34 +0300

    Change application ID and namespace for Android app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.student_grades_scanner"
+    namespace = "com.example.scanner_grades_control"
     compileSdk = 35
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.student_grades_scanner"
+        applicationId = "com.example.scanner_grades_control"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 21
